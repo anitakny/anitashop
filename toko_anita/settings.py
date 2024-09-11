b664f4792326c7db1801ac46a0da46a9a196cdf5
anitakny <antkhrnns9@gmail.com>
2024-09-11 11:36:43 +0700

    Tugas 2

--- toko_anita/settings.py
+++ toko_anita/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-q7#l)gtvnvjox@%f-sm@a%3sui3tg=@4b)gr@$%5kbc*$tz$0y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/anita.khoirun/tokoanita"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "anita-khoirun-tokoanita.pbp.cs.ui.ac.id"]
 
 
 # Application definition
